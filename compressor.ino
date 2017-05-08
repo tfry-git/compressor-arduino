c0c5be886136551964449bbcec3df42881cf3a83
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2017-05-08 20:49:38 +0200

    Add copyright/licence info

--- compressor.ino
+++ compressor.ino
@@ -1,3 +1,21 @@
+/********************************************************************************
+Arduino-based audio compressor
+Copyright (C) 2017  Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+********************************************************************************/
+
 //// main compressor parameters. Adjust these to your needs. ////
 int attack_f = 10;  // attack period (how soon the compressor will start attenuating loud signals) given in measurement frame
                     // units (see window_ms). Default setting corresponds to 50ms. Max buf_len / 2. Min 4.
